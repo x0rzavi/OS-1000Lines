f923a12a8f92ffe27fb7dabb7c48199d352110ad
Avishek Sen <x0rzavi@gmail.com>
2025-01-18 19:45:45 +0530

    implement kernel panic

--- kernel.c
+++ kernel.c
@@ -31,6 +31,10 @@ void putchar(char ch) {
 
 void kernel_main(void) {
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
+
+  PANIC("booted!");
+  printf("unreachable here!\n");
+
   printf("\n\nHello %s\n", "World!");
   printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
