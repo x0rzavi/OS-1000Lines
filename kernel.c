4c273a9e459515d734529c5f90ce5f9c8897d0e0
Avishek Sen <x0rzavi@gmail.com>
2025-01-23 11:58:56 +0530

    implement processes & context switching

--- kernel.c
+++ kernel.c
@@ -118,6 +118,122 @@ kernel_entry(void) { // entrypoint to kernel
                        "sret\n");
 }
 
+void handle_trap(struct trap_frame *f) { // trap handler function
+  uint32_t scause = READ_CSR(scause);
+  uint32_t stval = READ_CSR(stval);
+  uint32_t user_pc = READ_CSR(sepc);
+
+  PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval,
+        user_pc);
+}
+
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp) {
+  __asm__ __volatile__(
+      // Save callee-saved registers onto the current process's stack.
+      "addi sp, sp, -13 * 4\n" // Allocate stack space for 13 4-byte registers
+      "sw ra,  0  * 4(sp)\n"   // Save callee-saved registers only
+      "sw s0,  1  * 4(sp)\n"
+      "sw s1,  2  * 4(sp)\n"
+      "sw s2,  3  * 4(sp)\n"
+      "sw s3,  4  * 4(sp)\n"
+      "sw s4,  5  * 4(sp)\n"
+      "sw s5,  6  * 4(sp)\n"
+      "sw s6,  7  * 4(sp)\n"
+      "sw s7,  8  * 4(sp)\n"
+      "sw s8,  9  * 4(sp)\n"
+      "sw s9,  10 * 4(sp)\n"
+      "sw s10, 11 * 4(sp)\n"
+      "sw s11, 12 * 4(sp)\n"
+
+      // Switch the stack pointer.
+      "sw sp, (a0)\n" // *prev_sp = sp;
+      "lw sp, (a1)\n" // Switch stack pointer (sp) here
+
+      // Restore callee-saved registers from the next process's stack.
+      "lw ra,  0  * 4(sp)\n" // Restore callee-saved registers only
+      "lw s0,  1  * 4(sp)\n"
+      "lw s1,  2  * 4(sp)\n"
+      "lw s2,  3  * 4(sp)\n"
+      "lw s3,  4  * 4(sp)\n"
+      "lw s4,  5  * 4(sp)\n"
+      "lw s5,  6  * 4(sp)\n"
+      "lw s6,  7  * 4(sp)\n"
+      "lw s7,  8  * 4(sp)\n"
+      "lw s8,  9  * 4(sp)\n"
+      "lw s9,  10 * 4(sp)\n"
+      "lw s10, 11 * 4(sp)\n"
+      "lw s11, 12 * 4(sp)\n"
+      "addi sp, sp, 13 * 4\n" // We've popped 13 4-byte registers from the stack
+      "ret\n");
+}
+
+struct process procs[PROCS_MAX]; // All process control structures.
+
+struct process *create_process(uint32_t pc) {
+  // Find an unused process control structure.
+  struct process *proc = NULL;
+  int i;
+  for (i = 0; i < PROCS_MAX; i++) {
+    if (procs[i].state == PROC_UNUSED) {
+      proc = &procs[i];
+      break;
+    }
+  }
+
+  if (!proc)
+    PANIC("no free process slots");
+
+  // Stack callee-saved registers. These register values will be restored in
+  // the first context switch in switch_context.
+  uint32_t *sp = (uint32_t *)&proc->stack[sizeof(proc->stack)];
+  *--sp = 0;            // s11
+  *--sp = 0;            // s10
+  *--sp = 0;            // s9
+  *--sp = 0;            // s8
+  *--sp = 0;            // s7
+  *--sp = 0;            // s6
+  *--sp = 0;            // s5
+  *--sp = 0;            // s4
+  *--sp = 0;            // s3
+  *--sp = 0;            // s2
+  *--sp = 0;            // s1
+  *--sp = 0;            // s0
+  *--sp = (uint32_t)pc; // ra
+
+  // Initialize fields.
+  proc->pid = i + 1;
+  proc->state = PROC_RUNNABLE;
+  proc->sp = (uint32_t)sp;
+  return proc;
+}
+
+void delay(void) { // busy waiting
+  for (int i = 0; i < 100000000; i++)
+    __asm__ __volatile__("nop"); // do nothing
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+void proc_a_entry(void) { // process A entrypoint
+  printf("starting process A\n");
+  while (1) {
+    putchar('A');
+    switch_context(&proc_a->sp, &proc_b->sp);
+    delay();
+  }
+}
+
+void proc_b_entry(void) { // process B entrypoint
+  printf("starting process B\n");
+  while (1) {
+    putchar('B');
+    switch_context(&proc_b->sp, &proc_a->sp);
+    delay();
+  }
+}
+
 void kernel_main(void) { // what to be done by kernel
   memset(__bss, 0, (size_t)__bss_end - (size_t)__bss);
 
@@ -129,6 +245,10 @@ void kernel_main(void) { // what to be done by kernel
   paddr_t paddr1 = alloc_pages(1);
   printf("alloc_pages test: paddr0=%x\n", paddr0);
   printf("alloc_pages test: paddr1=%x\n", paddr1);
+
+  proc_a = create_process((uint32_t)proc_a_entry);
+  proc_b = create_process((uint32_t)proc_b_entry);
+  proc_a_entry(); // start running process A
 
   PANIC("booted!");
   // printf("unreachable here!\n");
@@ -152,13 +272,4 @@ boot(void) { // start booting OS from here
       : [stack_top] "r"(
           __stack_top) // pass the stack top address as %[stack_top]
   );
-}
-
-void handle_trap(struct trap_frame *f) { // trap handler function
-  uint32_t scause = READ_CSR(scause);
-  uint32_t stval = READ_CSR(stval);
-  uint32_t user_pc = READ_CSR(sepc);
-
-  PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval,
-        user_pc);
 }
--- kernel.h
+++ kernel.h
@@ -60,3 +60,15 @@ struct trap_frame {
   uint32_t s11;
   uint32_t sp;
 } __attribute__((packed));
+
+#define PROCS_MAX 8 // Maximum number of processes
+
+#define PROC_UNUSED 0   // Unused process control structure
+#define PROC_RUNNABLE 1 // Runnable process
+
+struct process {
+  int pid;             // Process ID
+  int state;           // Process state: PROC_UNUSED or PROC_RUNNABLE
+  vaddr_t sp;          // Stack pointer
+  uint8_t stack[8192]; // Kernel stack
+};
